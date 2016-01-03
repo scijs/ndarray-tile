28afbe0e8ea442e61d9bd7ef0ff2ee4a3a703938
Ricky Reusser <rsreusser@gmail.com>
2016-01-03 09:33:45 -0500

    Code comments

--- index.js
+++ index.js
@@ -27,11 +27,18 @@ function ndarrayTile () {
   var inputShape = input.shape.slice(0)
   var newShape = inputShape.slice(0)
   var newDim = Math.max(newShape.length, reps.length)
+
   for (i = 0; i < newDim; i++) {
+    // Assume unspecified dimensions has length 1:
     inputShape[i] = inputShape[i] === undefined ? 1 : inputShape[i]
+
+    // Unspecified reps have shape 1:
     reps[i] = reps[i] === undefined ? 1 : reps[i]
+
+    // Calculate the new total shape:
     newShape[i] = (newShape[i] === undefined ? 1 : newShape[i]) * reps[i]
 
+    // Disallow zero:
     if (newShape[i] === 0) {
       throw new Error('Number of tiles must be greater than zero')
     }
@@ -42,31 +49,47 @@ function ndarrayTile () {
     output = pool.zeros(newShape, input.dtype)
   }
 
+  // Calculate the total number of repetitions:
   var repProd = 1
   for (i = 0; i < reps.length; i++) {
     repProd *= reps[i]
   }
 
+  // Allocate an array that stores the current offset in each dim:
   var origin = new Array(reps.length)
   for (i = 0; i < reps.length; i++) {
     origin[i] = 0
   }
 
+  // Create an array that will do the slicing:
+  var toPick = newShape.slice(0)
+  for (d = 0; d < newShape.length; d++) {
+    toPick[d] = d < input.dimension ? null : 0
+  }
+
+  // Iterate over the n-dimensional repetitions:
   for (i = 0; i < repProd; i++) {
-    var toPick = newShape.slice(0)
-    for (d = 0; d < newShape.length; d++) {
-      toPick[d] = d < input.dimension ? null : origin[d]
+    // Only the additional tacked on dimensions need to be sliced:
+    for (d = input.dimension; d < newShape.length; d++) {
+      toPick[d] = origin[d]
     }
 
+    // Slice to reduce dimsionality to that of the input:
     var slice = output.pick.apply(output, toPick)
+
+    // Take a section of same shape as input:
     slice = slice.lo.apply(slice, origin)
     slice = slice.hi.apply(slice, input.shape)
 
+    // Copy input to output:
     ops.assign(slice, input)
 
-    // Skip index update on last iteration:
+    // Skip origin update on last iteration:
     if (i === repProd - 1) break
 
+    // Increment the origin in the trailing dimension. Cascade that
+    // to the next dimension if we're at the end. (In othre words,
+    // loop over the tiles in n-dimensions):
     var d = newDim - 1
     while (d >= 0) {
       origin[d] += inputShape[d]
